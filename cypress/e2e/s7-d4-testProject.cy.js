f96ef1c7bb39b34ffb21cb155b6b8c0dae687eaf
berk-karademir <berk.karademir@hotmail.com>
2024-11-01 16:04:40 +0300

    added script to package.json
    script to package.json:
    "cypress": "npx cypress open",
    "start": "concurrently \"npm run dev\" \"npm run cypress\""
    added to run cypress app and local host at the same time

--- cypress/e2e/s7-d4-testProject.cy.js
+++ cypress/e2e/s7-d4-testProject.cy.js
@@ -69,41 +69,11 @@ describe('My First Test', () => {
 
 ... cy.get('[data-cy="submit"]').click()
 */
-/*
-describe('Form Doldurma Testleri', () => {
-  beforeEach(() => {
-    // Testler başlamadan önce localhost'ta form sayfasına git
-    cy.visit('http://localhost:5173/'); // Uygulamanızın çalıştığı portu belirtin
-  });
 
-  it('Başarılı form doldurulduğunda success sayfası açılıyor', () => {
-    cy.get('#email').type('test@example.com'); // email alanına doğru bir email yaz
-    cy.get('#password').type('correctpassword'); // password alanına doğru bir şifre yaz
-    cy.get('#terms').check(); // Kullanım koşullarını kabul et
-    
-    cy.get('#submit').click(); // Gönder butonuna tıkla
-    cy.url().should('include', '/success'); // success sayfasının açıldığını kontrol et
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:5173");
   });
+});
 
-  it('Hatalı email girildiğinde doğru hata mesajı görünüyor ve buton disabled kalıyor', () => {
-    cy.get('#email').type('wrong-email'); // Hatalı bir email gir
-    cy.get('#submit').should('be.disabled'); // Gönder butonunun disabled olduğunu kontrol et
-    cy.get('.error-message').should('have.length', 1); // Sadece 1 hata mesajı var mı kontrol et
-    cy.contains('Lütfen geçerli bir email adresi girin'); // Beklenen hata mesajının doğru olduğunu kontrol et
-  });
-
-  it('Email ve password yanlış girildiğinde iki hata mesajı görüntüleniyor', () => {
-    cy.get('#email').type('wrong-email'); // Hatalı bir email gir
-    cy.get('#password').type('short'); // Hatalı bir şifre gir
-    cy.get('#submit').should('be.disabled'); // Gönder butonunun disabled olduğunu kontrol et
-    cy.get('.error-message').should('have.length', 2); // İki hata mesajı var mı kontrol et
-    cy.contains('Lütfen geçerli bir email adresi girin'); // Email için hata mesajını kontrol et
-    cy.contains('Şifre en az 8 karakter olmalıdır'); // Şifre için hata mesajını kontrol et
-  });
-
-  it('Email ve password doğru ama kurallar kabul edilmediyse buton disabled durumda kalıyor', () => {
-    cy.get('#email').type('test@example.com'); // Doğru email gir
-    cy.get('#password').type('correctpassword'); // Doğru şifre gir
-    cy.get('#submit').should('be.disabled'); // Kurallar kabul edilmediği için butonun disabled olduğunu kontrol et
-  });
-}); */
+  
